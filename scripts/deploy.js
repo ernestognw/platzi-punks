ea46ae9334c009752b4fa8fdee57a4f4b37b425f
ernestognw <ernestognw@gmail.com>
2021-09-19 19:35:32 -0500

    class(5): Added Open Zeppelin ERC721

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const deploy = async () => {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+  const deployed = await PlatziPunks.deploy();
+
+  console.log("Platzi Punks address:", deployed.address);
+};
+
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
